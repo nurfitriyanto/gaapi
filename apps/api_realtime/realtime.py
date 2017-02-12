91e0a701941d7de5e52a9010895be2938aa81a52
nurfitriyanto <nurfitriyanto13@gmail.com>
2017-02-13 01:27:50 +0700

    realtime

--- apps/api_realtime/realtime.py
+++ apps/api_realtime/realtime.py
@@ -1,4 +1,4 @@
-#! appps/api_report/report.py
+#! appps/api_reltime/realtime.py
 
 import traceback, sys
 from flask import jsonify
@@ -6,18 +6,16 @@ from flask_restful import Resource, url_for, request
 from service import manager
 from utils import msg_error
 
-class ReportRes(Resource):
+class RealtimeRes(Resource):
     def get (self):
         try:
             smanager = manager()
             profileId = request.args.get('profileId')
-            # Use the Analytics Service Object to query the Core Reporting API
-            # for the number of sessions in the past seven days.
-            output = smanager.data().ga().get(
+            # Use the Analytics Service Object to query the Core Realtime API
+            output = smanager.data().realtime().get(
                 ids='ga:' + profileId,
-                start_date='7daysAgo',
-                end_date='today',
-                metrics='ga:sessions'
+                metrics='rt:activeUsers',
+                dimensions='rt:userType,rt:browser, rt:operatingSystem, rt:deviceCategory, rt:country'
             ).execute()
                 
             return jsonify(output)
--- apps/routes.py
+++ apps/routes.py
@@ -2,11 +2,13 @@
 
 from flask import Blueprint
 from flask_restful import Api, Resource, url_for
-from apps.api_report.report import ReportRes
 from apps.api_users.users import UsersRes
+from apps.api_report.report import ReportRes
+from apps.api_realtime.realtime import RealtimeRes
 
 apis = Blueprint('apis', __name__, url_prefix='/api/v1')
 
 api = Api(apis)
-api.add_resource(ReportRes, '/report')
 api.add_resource(UsersRes, '/user')
+api.add_resource(ReportRes, '/report')
+api.add_resource(RealtimeRes, '/realtime')
